bf1cae6ace30c68885fd27cd6e6b901179189632
tudstlennkozh <arawkre64@gmail.com>
2020-05-15 18:32:14 +0200

    fixed version in settup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='maylibre',
-    version='0.1.0',
+    version='0.0.1',
     packages=[''],
     scripts=['maylibre.py'],
     install_requires=['exchangelib>=3.2.0'],
