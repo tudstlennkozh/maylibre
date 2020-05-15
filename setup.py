652f557f03a7b8a51fa0ee1e76b5a6bc38504f81
tudstlennkozh <arawkre64@gmail.com>
2020-05-15 14:55:11 +0200

    fixed wrong config in setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='maylibre',
     version='0.1',
     packages=[''],
+    scripts=['maylibre.py'],
     url='https://github.com/tudstlennkozh/maylibre/',
     license='Apache 2.0',
     author='tudstlennkozh',
