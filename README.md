bfbb5b86279c9aae6ec54dc4620a588211544cc3
tudstlennkozh <arawkre64@gmail.com>
2020-05-15 14:03:12 +0200

    first revision with almost complete code

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# maylibre
-Eases mailing from LibreOffice to an Exchange server
+<h2 align="center">
+    MayLibre
+</h2>
+<h3 align="center">
+    Eases mailing from LibreOffice to an Exchange server
+</h3>
+<h4 align="center">
+  <a href="#description">Description</a> |
+  <a href="#installation">Installation</a> |
+  <a href="#usage">Usage</a> |
+  <a href="#tests">Tests</a> |
+  <a href="#licensing">Licensing</a> |
+  <a href="#credits">Credits</a> |
+  <a href="#release-history">Release History</a>
+</h4>
+
+## Description
+
+If you ever wanted to :
+
+* send mass mailing from [LibreOffice](https://www.libreoffice.org/)
+* not setup a smtp server
+* rely on [Exchange Web Server](https://en.wikipedia.org/wiki/Microsoft_Exchange_Server)
+
+this script is made for you !
+
+![screenshot](media/screenshot.png)
+
+This script runs as a local smtp server, allowing to use mass mailing in LibreOffice, but sends all emails via an Exchange Web Server, just as if you were using it directly from LibreOffice.
+
+Of course, if your server already exposes smtp, you don't need it.
+
+This script does almost nothing, all work is done by the [exchangelib](https://pypi.org/project/exchangelib/) package. This script just glue together a local smtp server and the exchange server.
+
+
+## Installation
+
+![Python 3.8](https://img.shields.io/badge/python-3.8-blue) 
+
+To install MayLibre, prefer to do so in a virtual environment, then activate it and type the command:
+
+    md maylibre
+    cd maylibre
+    pip install https://github.com/tudstlennkozh/maylibre/zipball/master
+
+You then have to install needed package :
+
+```
+cd 
+pip install -r requirements.txt
+```
+
+That's it !
+
+## Usage
+
+modify the variables at the begining of the script to suit your EWS account  :
+
+```python
+# Connection details
+server: str = 'ews.com'
+e_mail: str = 'unkwnon@ewsmail.com'
+username: str = 'DOMAIN\\login'
+```
+
+then :
+
+    python maylibre.py
+
+enter the password associated with the account, and proceed to LibreOffice to send all your emails via your local smtp server directly connected to EWS.
+
+Just type Ctrl+C to end the script when done.
+
+## Tests
+
+This script has been tested on Windows 10, with Python 3.8 and LibreOffice 6.4 on Exchange 2016 server. Please let us know if any other environment is working.
+
+## Licensing
+
+[![Apache 2.0](https://img.shields.io/badge/license-Apache-blue)](/LICENSE)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at 
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+## Credits
+
+Please refer to the included [AUTHORS](/AUTHORS.md) for the full credits. Nothing would have been possible without [exchangelib](https://pypi.org/project/exchangelib/).
+
+## Release History
+
+Please refer to the included [CHANGELOG](/CHANGELOG.md) for the full release
+history.
+
+-------------------------
+###### © 2020 tudstlennkozh
+
---
+++ maylibre.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2020 tudstlennkozh
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import smtpd
+import email
+import email.header
+import getpass
+import asyncore
+
+from datetime import datetime
+from exchangelib import DELEGATE, IMPERSONATION, Account, Credentials, OAuth2Credentials, \
+    OAuth2AuthorizationCodeCredentials, FaultTolerance, Configuration, NTLM, GSSAPI, SSPI, \
+    OAUTH2, Build, Version, Message, Mailbox, FileAttachment, HTMLBody
+from exchangelib.autodiscover import AutodiscoverProtocol
+
+
+def is_probably_html(_s: str, content_type: str) -> bool:
+    if content_type.lower() == "text/html":
+        return True
+    return False
+
+
+def decode_mime_words(s):
+    return u''.join(
+        word.decode(encoding or 'utf8')
+        for word, encoding in email.header.decode_header(s))
+
+
+class CustomSMTPServer(smtpd.SMTPServer):
+
+    no = 0
+
+    def __init__(self, localaddr, account):
+        super().__init__(localaddr, None) #, enable_SMTPUTF8=True)
+        self.account = account
+
+    def _save_message(self, data):
+        filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}-{self.no}.eml"
+        with open(filename, 'wb') as f:
+            f.write(data)
+        print(f"{filename} saved." )
+        self.no += 1
+
+    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
+        print('Receiving message from:', peer)
+        print('Message addressed from:', mailfrom)
+        print('Message addressed to  :', rcpttos)
+        print('Message length        :', len(data))
+
+        self._save_message(data)
+
+        msg = email.message_from_bytes(data)
+
+        if msg.is_multipart():
+            print("multipart mail, ignored!")
+        else:
+            subject = decode_mime_words(msg.get('subject', ''))
+            body = msg.get_payload(decode=True).decode('utf8')
+            charset = msg.get_content_charset()
+            content_encoding = msg.get('content-transfer-encoding', '')
+            content_type = msg.get_content_type()
+
+            print('Message subject       :', subject)
+            print('Message charset       :', charset)
+            print('Message encoding      :', content_encoding)
+            print('Message type          :', content_type)
+
+            # now to EWS ...
+            if is_probably_html(body, content_type):
+                body = HTMLBody(body)
+
+            recipients = [Mailbox(email_address=r) for r in rcpttos]
+
+            m = Message(
+                account=self.account,
+                folder=self.account.sent,
+                subject=subject,
+                body=body,
+                to_recipients=recipients
+            )
+            m.send_and_save()
+            print("mail sent via Exchange")
+
+
+def connect(server, e_mail, username, password, auth_type=NTLM, access_type=DELEGATE):
+    """
+    Get Exchange account connection with server
+    """
+    creds = Credentials(username=username, password=password)
+    config = Configuration(server=server, credentials=creds, auth_type=auth_type)
+    return Account(primary_smtp_address=e_mail, autodiscover=True,
+                   config = config, credentials=creds, access_type=access_type)
+
+
+def display_account_infos(account):
+    """
+    Display some stats about this account
+    (suppose account is a valid live object)
+    """
+    print(account.mail_tips)
+    i = account.inbox
+    print(i.all())
+    print("global stats for inbox:")
+    for attr in ['total_count', 'unread_count']:
+        print("%s:%s" % (attr, getattr(i,attr)))
+
+
+def run(server, e_mail, username, password, port):
+    print(f"connecting to {server} ...")
+    account = connect(server, e_mail, username, password)
+
+    display_account_infos(account)
+    account.root.refresh()
+
+    s = CustomSMTPServer(('localhost', port), account)
+    print(f"listening on port {s.addr[1]} ...")
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        print("Interrupted !")
+        pass
+
+if __name__ == '__main__':
+    # Connection details
+    server: str = 'ews.com'
+    e_mail: str = 'unkwnon@ewsmail.com'
+    username: str = 'DOMAIN\\login'
+    password = getpass.getpass(f"Password for {username}:")
+
+    run(server, e_mail, username, password, 1025)
