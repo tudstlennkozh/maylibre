e37b39111d1386ca63dadaaaf13d483f161a65ab
tudstlennkozh <arawkre64@gmail.com>
2020-05-15 14:13:10 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='maylibre',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/tudstlennkozh/maylibre/',
+    license='Apache 2.0',
+    author='tudstlennkozh',
+    author_email='',
+    description='Eases mailing from LibreOffice to an Exchange server'
+)
