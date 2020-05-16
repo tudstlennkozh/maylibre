24a90155861f53ea4ed97ce3e62e8bc898a4649c
tudstlennkozh <arawkre64@gmail.com>
2020-05-16 17:44:39 +0200

    moved to 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [0.0.3] - 2020 xxx
+
+### Added
+
+- 
+
+### Changed
+
+- 
+
 ## [0.0.2] - 2020-05-16
 
 ### Changed
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='maylibre',
-    version='0.0.2',
+    version='0.0.3',
     packages=[''],
     install_requires=['exchangelib>=3.2.0'],
     url='https://github.com/tudstlennkozh/maylibre/',
