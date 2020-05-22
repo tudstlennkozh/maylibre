9056ad8b843b703ab8986ab846463ec9ccd9ff80
tudstlennkozh <arawkre64@gmail.com>
2020-05-22 15:41:20 +0200

    current is now 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='maylibre',
-    version='0.0.4',
+    version='0.0.5',
     packages=[''],
     install_requires=['exchangelib'],
     url='https://github.com/tudstlennkozh/maylibre/',
